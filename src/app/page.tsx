5db30bc0831a9331cd1eafa4ef5527ee89f74757
douglaschalegre <douglas.chalegre@gmail.com>
2024-02-01 16:26:10 -0300

    feature: Add welcome message and button to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-4xl font-bold'>Welcome to Timebox</h1>
+      <Image
+        src='/box-open-solid.svg'
+        alt='Timebox logo'
+        width={200}
+        height={200}
+      />
+      <div className='flex flex-col items-center gap-y-1'>
+        <p className='text-lg text-center'>Manage your time with Timebox</p>
+        <a href='/app'>
+          <Button className='w-min'>Get started</Button>
+        </a>
+      </div>
+    </main>
   );
 }
